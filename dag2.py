3ea265932c8f2f52fad7743c7fd4253d646ce335
Miguel Angel Manuttupa Ligas <manuttupa.ligas@gmail.com>
2025-11-14 23:22:01 -0500

    Update dag2.py

--- dag2.py
+++ dag2.py
@@ -2,24 +2,27 @@ import pendulum
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
-from airflow.providers.airflow.utils.task_group import TaskGroup
-from airflow.operators.dagrun_operator import TriggerDagRunOperator
-from airflow.utils.dates import days_ago
+from airflow.sdk import TaskGroup
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 LIMA = pendulum.timezone("America/Lima")
 with DAG(
-    dag_id='dag_2',
-    schedule=None, catchup=False,
-    start_date=pendulum.datetime(2025, 1, 1, tz=LIMA)
+  dag_id='dag_2',
+  schedule=None, catchup=False,
+  start_date=pendulum.datetime(2025, 1, 1, tz=LIMA)
 ) as dag:
+  
   start_task = EmptyOperator(task_id="start_task")
-  with TaskGroup("task_group_2", dag=dag2) as tg2:
-      task_1 = EmptyOperator(task_id="task_1")
-      task_2 = EmptyOperator(task_id="task_2")
+
+  with TaskGroup("task_group_2") as tg2:
+    task_1 = EmptyOperator(task_id="task_1")
+    task_2 = EmptyOperator(task_id="task_2")
+
+    task_1 >> task_2
+
   trigger_dag3 = TriggerDagRunOperator(
-      task_id="trigger_dag3",
-      trigger_dag_id="dag_3"
+    task_id="trigger_dag3",
+    trigger_dag_id="dag_3"
   )
 
-# Dependencias
-start_task >> tg2 >> trigger_dag3
+  start_task >> tg2 >> trigger_dag3
