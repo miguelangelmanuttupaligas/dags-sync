059cb76e6ca09bb5bb05f659eb1bf242c1fa1d5d
Miguel Angel Manuttupa Ligas <manuttupa.ligas@gmail.com>
2025-11-14 23:02:39 -0500

    Create DAG 3 with an empty operator task

---
+++ dag3.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.dates import days_ago
+
+# Definimos el DAG 3
+with DAG(dag_id="dag_3",
+         schedule_interval=None,
+         start_date=days_ago(1),
+         catchup=False,
+) as dag:
+  # Tareas del DAG 3
+  task_1 = EmptyOperator(task_id="task_1")
+
+task_1
