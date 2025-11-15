47759136da5ada1b392e2e2422fbda1520941801
Miguel Angel Manuttupa Ligas <manuttupa.ligas@gmail.com>
2025-11-14 23:30:18 -0500

    Update dag3.py

--- dag3.py
+++ dag3.py
@@ -2,6 +2,7 @@ import pendulum
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.bash import BashOperator
 from airflow.sdk import TaskGroup
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
@@ -13,5 +14,6 @@ with DAG(
 ) as dag:
   
   task_1 = EmptyOperator(task_id="task_1")
+  task_2 = BashOperator(task_id="task_2",bash_commad='echo "Me encuentro en DAG 3"')
 
   task_1
