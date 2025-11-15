3c11c80cdfee95c7ffbc0a76a8b068caf36dae2d
Miguel Angel Manuttupa Ligas <manuttupa.ligas@gmail.com>
2025-11-14 23:05:24 -0500

    Update import path for EmptyOperator in dag3.py

--- dag3.py
+++ dag3.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.utils.dates import days_ago
 
 # Definimos el DAG 3
