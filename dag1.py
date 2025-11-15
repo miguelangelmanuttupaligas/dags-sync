b576875009631ea89c3e8a9c16b589eb8fe7d599
Miguel Angel Manuttupa Ligas <manuttupa.ligas@gmail.com>
2025-11-14 23:09:09 -0500

    Update dag1.py

--- dag1.py
+++ dag1.py
@@ -1,26 +1,25 @@
+import pendulum
+from datetime import datetime
 from airflow import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
-from airflow.utils.dates import days_ago
 from airflow.providers.airflow.utils.task_group import TaskGroup
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
 
-# Definimos el DAG 1
-with DAG(dag_id="dag_1",
-         schedule_interval=None,
-         start_date=days_ago(1),
-         catchup=False,
+LIMA = pendulum.timezone("America/Lima")
+with DAG(
+    dag_id='dag_1',
+    schedule=None, catchup=False,
+    start_date=pendulum.datetime(2025, 1, 1, tz=LIMA)
 ) as dag:
-  # Tareas del DAG 
   start_task = EmptyOperator(task_id="start_task")
-  # Usamos un TaskGroup para agrupar tareas
   with TaskGroup("task_group_1", dag=dag1) as tg1:
-      task_1 = EmptyOperator(task_id="task_1")
-      task_2 = EmptyOperator(task_id="task_2")
-      task_3 = EmptyOperator(task_id="task_3")
-  # Invocar el DAG 2
+    task_1 = EmptyOperator(task_id="task_1")
+    task_2 = EmptyOperator(task_id="task_2")
+    task_3 = EmptyOperator(task_id="task_3")
+
   trigger_dag2 = TriggerDagRunOperator(
-      task_id="trigger_dag2",
-      trigger_dag_id="dag_2",  # El DAG que se invoca
+    task_id="trigger_dag2",
+    trigger_dag_id="dag_2",  # El DAG que se invoca
   )
 # Dependencias
 start_task >> tg1 >> trigger_dag2
