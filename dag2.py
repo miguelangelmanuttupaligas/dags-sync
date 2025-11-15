48668c6fd5b284fdea51a439f82006b99e8f8871
Miguel Angel Manuttupa Ligas <manuttupa.ligas@gmail.com>
2025-11-14 23:09:11 -0500

    Refactor DAG 2 to use pendulum for timezone
    Updated the DAG definition to use pendulum for timezone and changed the start date to January 1, 2025.

--- dag2.py
+++ dag2.py
@@ -1,21 +1,21 @@
+import pendulum
+from datetime import datetime
 from airflow import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.airflow.utils.task_group import TaskGroup
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
 from airflow.utils.dates import days_ago
 
-# Definimos el DAG 2
-with DAG(dag_id="dag_2",
-         schedule_interval=None,
-         start_date=days_ago(1),
-         catchup=False,
+LIMA = pendulum.timezone("America/Lima")
+with DAG(
+    dag_id='dag_2',
+    schedule=None, catchup=False,
+    start_date=pendulum.datetime(2025, 1, 1, tz=LIMA)
 ) as dag:
-  # Tareas del DAG 2
   start_task = EmptyOperator(task_id="start_task")
   with TaskGroup("task_group_2", dag=dag2) as tg2:
       task_1 = EmptyOperator(task_id="task_1")
       task_2 = EmptyOperator(task_id="task_2")
-  # Invocar el DAG 3
   trigger_dag3 = TriggerDagRunOperator(
       task_id="trigger_dag3",
       trigger_dag_id="dag_3"
